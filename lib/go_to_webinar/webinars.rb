a4c142596f4986442af3d3eac19129de5a9eb316
nathanwfish <nathanwfish@gmail.com>
2012-04-09 09:53:21 -0700

    adding tests

--- lib/go_to_webinar/webinars.rb
+++ lib/go_to_webinar/webinars.rb
@@ -2,23 +2,23 @@ module GoToWebinar
   module Webinars
     
     def get_historical_webinars(params)
-      self.class.get("historicalWebinars", :query => params).parsed_response
+      self.class.get("historicalWebinars", :query => params)
     end
     
     def get_upcoming_webinars(params)
-      self.class.get("upcomingWebinars", :query => params).parsed_response
+      self.class.get("upcomingWebinars", :query => params)
     end
     
     def get_webinar(webinar_key)
-      self.class.get("webinars/#{webinar_key}").parsed_response
+      self.class.get("webinars/#{webinar_key}")
     end
     
     def get_webinar_meeting_times(webinar_key)
-      self.class.get("webinars/#{webinar_key}/meetingTimes").parsed_response
+      self.class.get("webinars/#{webinar_key}/meetingTimes")
     end
     
     def get_webinars()
-      self.class.get("webinars").parsed_response
+      self.class.get("webinars")
     end
     
   end
--- test/test_attendees.rb
+++ test/test_attendees.rb
@@ -5,27 +5,48 @@ class AttendeesTest < Test::Unit::TestCase
   context "methods" do
 
     setup do
-      @c = GoToWebinar::API.new("12345", "54321")
+      @c = GoToWebinar::API.new("54321", "12345")
     end
 
     should "generate valid get attendee" do
-      assert true
+      FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/12345/webinars/12345/sessions/12345/attendees/54321",:body => '{"registrantKey": "54321"}', :content_type => "application/json", :status => ["200", "OK"])
+      @r = @c.get_attendee("12345","12345","54321")
+      assert_not_nil @r
+      assert @r.parsed_response.is_a?(Hash)
+      assert_not_nil @r.parsed_response["registrantKey"]
+      
     end
     
-    should "generate valid attendee poll questions" do
-      assert true
+    should "generate valid attendee poll answers" do
+      FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/12345/webinars/12345/sessions/12345/attendees/54321/polls",:body => '[{"question":"something"}]', :content_type => "application/json", :status => ["200", "OK"])
+      @r = @c.get_attendee_poll_answers("12345","12345","54321")
+      assert_not_nil @r
+      assert @r.parsed_response.is_a?(Array)
+      assert_not_nil @r.parsed_response.first["question"]
     end
     
     should "generate valid get attendee questions" do
-      assert true
+      FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/12345/webinars/12345/sessions/12345/attendees/54321/questions",:body => '[{"question":"something"}]', :content_type => "application/json", :status => ["200", "OK"])
+      @r = @c.get_attendee_questions("12345","12345","54321")
+      assert_not_nil @r
+      assert @r.parsed_response.is_a?(Array)
+      assert_not_nil @r.parsed_response.first["question"]
     end
     
     should "generate valid get attendee survey answers" do
-      assert true
+      FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/12345/webinars/12345/sessions/12345/attendees/54321/surveys",:body => '[{"question":"something"}]', :content_type => "application/json", :status => ["200", "OK"])
+      @r = @c.get_attendee_survey_answers("12345","12345", "54321")
+      assert_not_nil @r
+      assert @r.parsed_response.is_a?(Array)
+      assert_not_nil @r.parsed_response.first["question"]
     end
     
     should "generate valid get attendees for all webinar sessions" do
-      assert true
+      FakeWeb.register_uri(:get, "https://api.citrixonline.com/G2W/rest/organizers/12345/webinars/12345/attendees",:body => '[{"question":"something"}]', :content_type => "application/json", :status => ["200", "OK"])
+      @r = @c.get_attendees_for_all_webinar_sessions("12345")
+      assert_not_nil @r
+      assert @r.parsed_response.is_a?(Array)
+      assert_not_nil @r.parsed_response.first["question"]
     end
     
   end
